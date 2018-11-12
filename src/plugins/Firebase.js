211f480725b608eb42abc6b2d9f2aada1cd8ef39
Валера Любимов <v.udav@bk.ru>
2018-11-12 03:02:18 +0300

    Init firebase

---
+++ src/plugins/Firebase.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import VueFire from "vuefire";
+import firebase from "firebase";
+
+Vue.use(VueFire);
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+});
+const db = firebaseApp.database();
+
+export default db;
