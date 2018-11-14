b2e61d9de2a630d793434c0f5b1bb8c21b119b95
Валера Любимов <v.udav@bk.ru>
2018-11-14 20:02:03 +0300

    Add logo

--- src/components/layouts/MainLayout/Header.vue
+++ src/components/layouts/MainLayout/Header.vue
@@ -1,3 +1,19 @@
 <template>
-  <el-header>Header</el-header>
+  <el-header class="header" height="auto">
+    <div class="logo">open street art</div>
+  </el-header>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2px;
+  padding-bottom: 20px;
+}
+
+.logo {
+  font-size: 50px;
+}
+</style>
